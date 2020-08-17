5bfdd2a34ba5ba93f04dda934d9416251e4758ee
Danang Nurdiansyah <dnurdiansyah@allied.local>
2020-08-17 13:19:20 +0800

    commit on master

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -39,5 +39,6 @@ export class NavComponent implements OnInit {
   logout() {
     localStorage.removeItem('token');
     this.alertify.message('Logged out');
+    this.router.navigate(['/home']);
   }
 }
